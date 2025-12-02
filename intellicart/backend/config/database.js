ab6a9bc910f166277422b7de6d3c78e0013b9f6f
codergangs5 <codergangs4@gmail.com>
2025-12-02 17:54:48 +0530

    fix mongo connection

--- intellicart/backend/config/database.js
+++ intellicart/backend/config/database.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 const MONGO_URI = process.env.MONGO_URI;
 
-const connectDatabase = () => {
-    mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => {
-            console.log("Mongoose Connected");
-        });
+const connectDatabase = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB Connected Successfully");
+  } catch (err) {
+    console.log("MongoDB Connection Error:", err);
+    process.exit(1);
+  }
 }
 
 module.exports = connectDatabase;
