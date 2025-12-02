059e7ad4e46e86abdc69aea5a58fa9a7a8d11a3f
codergangs5 <codergangs4@gmail.com>
2025-12-02 11:19:02 +0530

    Added some product details

--- intellicart/backend/utils/sendEmail.js
+++ intellicart/backend/utils/sendEmail.js
@@ -1,52 +1,22 @@
-// const nodeMailer = require('nodemailer');
-// const sgMail = require('@sendgrid/mail')
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-// const sendEmail = async (options) => {
+const sendEmail = async (options) => {
 
-//     // const transporter = nodeMailer.createTransport({
-//     //     host: process.env.SMTP_HOST,
-//     //     port: process.env.SMTP_PORT,
-//     //     service: process.env.SMTP_SERVICE,
-//     //     auth: {
-//     //         user: process.env.SMTP_MAIL,
-//     //         pass: process.env.SMTP_PASSWORD,
-//     //     },
-//     // });
+    const msg = {
+        to: options.email,
+        from: process.env.SENDGRID_MAIL,
+        templateId: options.templateId,
+        dynamic_template_data: options.data,
+    }
 
-//     // const mailOptions = {
-//     //     from: process.env.SMTP_MAIL,
-//     //     to: options.email,
-//     //     subject: options.subject,
-//     //     html: options.message,
-//     // };
-
-//     // await transporter.sendMail(mailOptions);
-
-//     const msg = {
-//         to: options.email,
-//         from: process.env.SENDGRID_MAIL,
-//         templateId: options.templateId,
-//         dynamic_template_data: options.data,
-//     }
-//     sgMail.send(msg).then(() => {
-//         console.log('Email Sent')
-//     }).catch((error) => {
-//         console.error(error)
-//     });
-// };
-
-// module.exports = sendEmail;
-
-
-
-
-
-
-
-// Email system disabled temporarily
-const sendEmail = async () => {
-  console.log("Email sending is disabled temporarily.");
-};
+    try {
+        await sgMail.send(msg);
+        console.log('Email successfully sent');
+    } catch (error) {
+        console.log(error);
+        throw new Error(error.message);
+    }
+}
 
 module.exports = sendEmail;
