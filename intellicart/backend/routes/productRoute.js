8234f24339db150e19f8f50c127803e84e98b51d
codergangs5 <codergangs4@gmail.com>
2025-12-05 15:23:00 +0530

    Fix product routes handler issue

--- intellicart/backend/routes/productRoute.js
+++ intellicart/backend/routes/productRoute.js
@@ -1,25 +1,35 @@
-const express = require('express');
-const { getAllProducts, getProductDetails, updateProduct, deleteProduct, getProductReviews, deleteReview, createProductReview, createProduct, getAdminProducts, getProducts } = require('../controllers/productController');
-const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+const express = require("express");
+const {
+  getAllProducts,
+  getProducts,
+  getProductDetails,
+  getAdminProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/productController");
+
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
 const router = express.Router();
 
-router.route('/products').get(getAllProducts);
-router.route('/products/all').get(getProducts);
+// Public
+router.route("/products").get(getAllProducts);
+router.route("/product/:id").get(getProductDetails);
+router.route("/products/all").get(getProducts);
 
-router.route('/admin/products').get(isAuthenticatedUser, authorizeRoles("admin"), getAdminProducts);
-router.route('/admin/product/new').post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+// Admin
+router
+  .route("/admin/products")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAdminProducts);
 
-router.route('/admin/product/:id')
-    .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
-    .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+router
+  .route("/admin/product/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
 
-router.route('/product/:id').get(getProductDetails);
-
-router.route('/review').put(isAuthenticatedUser, createProductReview);
-
-router.route('/admin/reviews')
-    .get(getProductReviews)
-    .delete(isAuthenticatedUser, deleteReview);
+router
+  .route("/admin/product/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
 
 module.exports = router;
