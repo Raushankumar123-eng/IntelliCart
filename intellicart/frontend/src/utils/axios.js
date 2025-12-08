3dfaa3d0a0ac70f487e9a4d6efd22df572dc4fdc
codergangs5 <codergangs4@gmail.com>
2025-12-08 12:34:24 +0530

    Fix axios baseURL with /api/v1

--- intellicart/frontend/src/utils/axios.js
+++ intellicart/frontend/src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://intellicart.onrender.com"
+  baseURL: "https://intellicart.onrender.com/api/v1",
+  withCredentials: true,
 });
-
 
 export default API;
