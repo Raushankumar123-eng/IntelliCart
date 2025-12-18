7d76436b178224d30efe4c43be37510162848585
codergangs5 <codergangs4@gmail.com>
2025-12-18 12:24:06 +0530

    fixed forget password

--- intellicart/backend/utils/sendEmail.js
+++ intellicart/backend/utils/sendEmail.js
@@ -1,22 +1,31 @@
-const sgMail = require("@sendgrid/mail");
+// const sgMail = require("@sendgrid/mail");
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendEmail = async (options) => {
-  const msg = {
-    to: options.email,
-    from: process.env.SENDGRID_FROM_EMAIL, // ✅ FIXED
-    templateId: options.templateId,
-    dynamicTemplateData: options.data,     // ✅ correct key
-  };
+// const sendEmail = async (options) => {
+//   const msg = {
+//     to: options.email,
+//     from: process.env.SENDGRID_FROM_EMAIL, // ✅ FIXED
+//     templateId: options.templateId,
+//     dynamicTemplateData: options.data,     // ✅ correct key
+//   };
 
-  try {
-    await sgMail.send(msg);
-    console.log("Email successfully sent");
-  } catch (error) {
-    console.error("SendGrid Error:", error.response?.body || error.message);
-    throw new Error("Email could not be sent");
-  }
-};
+//   try {
+//     await sgMail.send(msg);
+//     console.log("Email successfully sent");
+//   } catch (error) {
+//     console.error("SendGrid Error:", error.response?.body || error.message);
+//     throw new Error("Email could not be sent");
+//   }
+// };
 
-module.exports = sendEmail;
+// module.exports = sendEmail;
+
+
+// await sendEmail({ ... });
+
+res.status(200).json({
+  success: true,
+  message: "TEST MODE: Forgot password working",
+});
+
