3d547694d84984db6c0e514b8961968fbf13410a
codergangs5 <codergangs4@gmail.com>
2025-12-25 14:55:42 +0530

    fixed protected route.js

--- intellicart/frontend/src/Routes/ProtectedRoute.js
+++ intellicart/frontend/src/Routes/ProtectedRoute.js
@@ -2,17 +2,20 @@ import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const ProtectedRoute = ({ children, isAdminRoute = false }) => {
-  const { isAuthenticated, user } = useSelector((state) => state.user);
+  const { isAuthenticated, user, loading } = useSelector(
+    (state) => state.user
+  );
+
+  if (loading) return null; // ⬅ IMPORTANT
 
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
   }
 
-  if (isAdminRoute && user.role !== "admin") {
+  if (isAdminRoute && user?.role !== "admin") {
     return <Navigate to="/login" replace />;
   }
 
   return children;
 };
 
-export default ProtectedRoute;
