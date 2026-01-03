e5e3da87ed3b46008423bc50189db77d47cb41d4
codergangs5 <codergangs4@gmail.com>
2026-01-03 18:23:14 +0530

    fixed cloudnary

--- intellicart/backend/server.js
+++ intellicart/backend/server.js
@@ -45,8 +45,9 @@ app.use((req, res, next) => {
 // ==============================
 // Body Parsers
 // ==============================
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
 
 // ==============================
 // Cookie Parser
